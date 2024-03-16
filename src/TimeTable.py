8a9c5be526203acd15bff1bdd3c610035cd926d4
Ore0 <59217639+Coziyu@users.noreply.github.com>
2024-03-16 21:40:40 +0800

    Added Event Classes

---
+++ src/TimeTable.py
@@ -0,0 +1,7 @@
+from Events import Priority, FixedEvent, DynamicEvent
+
+# Each TimeTable object is a collection of FixedEvent and DynamicEvent objects seperately.
+
+class TimeTable:
+    def __init__(self):
+        pass
