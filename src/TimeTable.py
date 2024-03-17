d92efe85a2c7ccd18d28ba90882d8576444d9d6a
Ore0 <59217639+Coziyu@users.noreply.github.com>
2024-03-18 00:04:46 +0800

    Added methods prototype for TimeTable

--- src/TimeTable.py
+++ src/TimeTable.py
@@ -1,7 +1,36 @@
 from Events import Priority, FixedEvent, DynamicEvent
+import datetime
+# TODO: Complete TimeTable class
+# TODO: Add function to get empty time ranges in the timetable
 
 # Each TimeTable object is a collection of FixedEvent and DynamicEvent objects seperately.
+# Balanced Load:
+# Load Factor = 5 * Min( [Free Time - Work : weekdays] ) + 2 * WeekEndScaleFactor * Min( [Free Time - Work : weekends] )
 
 class TimeTable:
     def __init__(self):
+        pass
+
+    def get_empty_time_ranges(self):
+        pass
+
+    def add_fixed_event(self, event: FixedEvent):
+        pass
+
+    def add_dynamic_event(self, event: DynamicEvent):
+        pass
+
+    def remove_fixed_event(self, id: int):
+        pass # Check equality id
+
+    def remove_dynamic_event(self, id: int):
+        pass # Check equality id
+
+    def get_fixed_events_by_week(self, week: datetime.date):
+        pass
+
+    def get_dynamic_events_by_week(self, week: datetime.date):
+        pass
+
+    def shedule_dynamic_events(self):
         pass
